3151b0825a74697ec7e41e8dcdf1570cb376b620
HeyKunal <60438567+HeyKunal@users.noreply.github.com>
2020-10-22 11:45:58 +0530

    Add files via upload

---
+++ Simple-Interest_(PYTHON).py
@@ -0,0 +1,6 @@
+P = int(input("enter the principal amount: "))
+T = int(input("enter the time: "))
+R = int(input("enter the rate: "))
+SI = (P * R * T) / 100
+print("simple interest is", SI)
+#by Kunal Vartak (72)
